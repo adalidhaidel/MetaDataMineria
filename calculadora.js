3654052adaa9998db833a56409a77c6c2dd83691
adalidhaidel <haidel_2005@hotmail.com>
2025-07-19 22:40:17 -0400

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,14 @@
+﻿function calcularRatio() {
+    const leyCabeza = parseFloat(document.getElementById('ley-cabeza').value);
+    const leyConcentrado = parseFloat(document.getElementById('ley-concentrado').value);
+    
+    if (isNaN(leyCabeza) || isNaN(leyConcentrado)) {
+        alert("Ingresa valores numéricos válidos");
+        return;
+    }
+
+    const ratio = (leyConcentrado / leyCabeza).toFixed(2);
+    document.getElementById('resultado-ratio').innerHTML = `
+        <strong>Ratio de Concentración:</strong> ${ratio}
+    `;
+}
